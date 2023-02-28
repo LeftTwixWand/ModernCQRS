6e0c6d9da5049dace317500555ba18bfd04b31fc
Vladyslav Horbachov <vhorbachov@microsoft.com>
2023-02-28 17:47:27 +0100

    Remove ComplexApp example

--- ComplexApp/ComplexApp.Application/Commands/MyResultCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using BuildingBlocks.Application.Commands;
-
-namespace ComplexApp.Application.Commands;
-
-public record MyResultCommand(string Text) : CommandBase<string>;
--- ComplexApp/ComplexApp.Application/Queries/MyQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using BuildingBlocks.Application.Queries;
-
-namespace ComplexApp.Application.Queries;
-
-public record MyQuery(string Text) : QueryBase<string>;
